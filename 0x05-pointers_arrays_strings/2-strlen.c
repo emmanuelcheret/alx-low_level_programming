8d552a25f040c41a3a764b05d1ae47079702bcd0
Emmanuel Cheret <emmanuelcherry7@gmail.com>
2023-02-27 09:32:37 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int_strlen - function that returns the lenght of a string
+ * @s: function to be returned
+ * Return: 0 on success
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
