3c804238a2a54372a8e08b12f1e8b8fbf8f4bd4b
Emmanuel Cheret <emmanuelcherry7@gmail.com>
2023-03-08 12:56:33 -0800

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if a number is a prime number
+ * @n: the number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+    int i;
+
+    if (n <= 1)
+        return (0);
+    if (n <= 3)
+        return (1);
+    if (n % 2 == 0 || n % 3 == 0)
+        return (0);
+    i = 5;
+    while (i * i <= n)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return (0);
+        i += 6;
+    }
+	return (1);
+}
