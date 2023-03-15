d38f698dc6c77c56027417b2a823f2536dd1d3d1
Emmanuel Cheret <emmanuelcherry7@gmail.com>
2023-03-15 04:06:05 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - Prints numberof arguments passed
+ * @argc: An array that prints arguments passed
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
