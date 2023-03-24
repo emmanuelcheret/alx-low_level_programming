6a3181ec62c2196fd621657cf6583bb760c556f5
Emmanuel Cheret <emmanuelcherry7@gmail.com>
2023-03-24 10:49:09 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name using a callback function to format the output
+ * @name: the name to print
+ * @f: the callback function to use for formatting
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
